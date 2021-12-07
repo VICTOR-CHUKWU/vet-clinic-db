8033490bafea0c0603da3978a3f5927ffda7f6be
victor-chukwu <okenwa1993@gmail.com>
2021-12-07 13:42:09 +0100

    First commit

---
+++ queries.sql
@@ -0,0 +1,5 @@
+select * from animals;
+select * from animals where name like '%ti';
+select * from animals where weight between '100' and '120';
+select * from animals where domestic = true;
+select name from animals where location= 'Africa';
